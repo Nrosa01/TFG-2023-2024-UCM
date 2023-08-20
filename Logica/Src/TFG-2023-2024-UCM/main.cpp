d7f37ffaa6ac1a86d1cd9a0c452ba33ae21cd2a1
Rioni <nrosa01@ucm.es>
2023-08-20 15:32:33 +0200

    Inicialización de GLFW y Glad para mostrar una ventana

--- Logica/Src/TFG-2023-2024-UCM/main.cpp
+++ Logica/Src/TFG-2023-2024-UCM/main.cpp
@@ -4,10 +4,50 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 // Main code
 int main(int, char**)
 {
-    std::cout << "Hello world" << std::endl;
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); //Hay que estar en Debug
+	
+	glfwInit();
 
-    return 0;
+	// Tell GLFW what version of OpenGL we are using (OpenGL 3.3)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Tell GLFW we are using core profile, that means we are using the modern functions without any compatibility with old ones
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window(glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "TFG-2023", nullptr, nullptr), glfwDestroyWindow);
+
+	if (window == nullptr)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window.get());
+
+	// Load GLAD so it configures OpenGL
+	gladLoadGL();
+
+	//Specify the viewport of OpenGL in the Window
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window.get());
+
+	while (!glfwWindowShouldClose(window.get()))
+	{
+		// Equivalent to SDL poll events
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
