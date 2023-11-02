98d366b72875540e053ebc082aa17de969f64ad0
Rioni <nrosa01@ucm.es>
2023-11-02 11:45:03 +0100

    Particle data struct members are now constant
    These data is not mutable by any means

--- Logica/Src/TFG-2023-2024-UCM/Particle.h
+++ Logica/Src/TFG-2023-2024-UCM/Particle.h
@@ -10,20 +10,20 @@ enum Direction { UP, DOWN, LEFT, RIGHT, UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT
 enum material { empty, sand, gas, water, rock, acid };
 
 struct PhysicsProperties {
-	int density;
-	int color;
-	int flammability;
-	int explosiveness;
-	int boilingPoint;
-	int startingTemperature;
+	const int density;
+	const int color;
+	const int flammability;
+	const int explosiveness;
+	const int boilingPoint;
+	const int startingTemperature;
 };
 
 struct ParticleData
 {
-	std::string text_id; // We might want to change this to char text_id[16] for performance reasons
-	uint8_t id; // We are not using 256 particles right?
-	ParticleProject::colour_t particle_color; // Should this be a physics property?
-	std::vector<ParticleProject::Vector2D> movement_passes; // We could just use Direction2D*, a vector has many stuff we don't really want that might be slow
+	const std::string text_id; // We might want to change this to char text_id[16] for performance reasons
+	const uint8_t id; // We are not using 256 particles right?
+	const ParticleProject::colour_t particle_color; // Should this be a physics property?
+	const std::vector<ParticleProject::Vector2D> movement_passes; // We could just use Direction2D*, a vector has many stuff we don't really want that might be slow
 	// PhysicsProperties physics_properties;
 };
 
@@ -32,7 +32,7 @@ struct Particle {
 	uint32_t type = 0;
 	
 	int temperature;
-	bool is_stagnant = false;
+	// bool is_stagnant = false;
 	
 	// Instead of using an external boolean array, we can just use a counter to check against that changes with each iteration
 	// This counter can be used to track update of particle groups
