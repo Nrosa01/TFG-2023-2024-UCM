b4326fbe1bc7d79633ec62ff3d2b0da1264d0305
Rioni <nrosa01@ucm.es>
2023-11-02 18:02:55 +0100

    ParticleData fields aren't const now. ParticleDataRegistry save text to id

--- Logica/Src/TFG-2023-2024-UCM/ParticleData.h
+++ Logica/Src/TFG-2023-2024-UCM/ParticleData.h
@@ -4,11 +4,12 @@
 #include "Colour.h"
 #include "Vector2D.h"
 
+// Keep fields non mutable even if they are not const	
 struct ParticleData
 {
-	const std::string text_id; // We might want to change this to char text_id[16] for performance reasons
-	const uint8_t id; // We are not using 256 particles right?
-	const ParticleProject::colour_t particle_color; // Should this be a physics property?
-	const std::vector<ParticleProject::Vector2D> movement_passes; // We could just use Direction2D*, a vector has many stuff we don't really want that might be slow
+	std::string text_id; // We might want to change this to char text_id[16] for performance reasons
+	uint8_t id; // We are not using 256 particles right?
+	ParticleProject::colour_t particle_color; // Should this be a physics property?
+	std::vector<ParticleProject::Vector2D> movement_passes; // We could just use Direction2D*, a vector has many stuff we don't really want that might be slow
 	// PhysicsProperties physics_properties;
 };
--- Logica/Src/TFG-2023-2024-UCM/ParticleDataRegistry.h
+++ Logica/Src/TFG-2023-2024-UCM/ParticleDataRegistry.h
@@ -20,9 +20,47 @@ public:
 		return instance;
 	}
 
-	void addParticleData(const ParticleData& data) {
+	void addParticleData(const ParticleData& data)
+	{
+		// Check if the text_id is already in the map
+		// It's possible that a interaction was defined with a particle that doesn't exist
+		// If the particle is registered, we overwrite it
+		if (text_to_id_map.find(data.text_id) != text_to_id_map.end()) {
+			const uint32_t index = text_to_id_map[data.text_id];
+
+			// Given the ParticleData fields are constant, we can't assign them directly by doing
+			// particle_data[index] = data (won't work)
+			// So there are 2 options
+			// 1- Making the fields non constant. GIven we always return a const reference in getParticleData, this 
+			// won't really be a problem buuuuut... ParticleData fields should be inmutable by design, it makes no sense that those data can chance
+			// 2- Removing the old data and inserting the new. This is way worse performance wise as is O(n), but this is a very specific situation that 
+			// might be called only in 1 frame in the entire lifetime. ParticleData vector won't be higher than 256 elements so it won't really have a noticeable impact on the simulation
+			//particle_data.erase(particle_data.begin() + index);
+			//particle_data.insert(particle_data.begin() + index, data);
+
+			// Finally, we chose the 1 option becaus we can't use the erase method, internally it calls the copy constructor which is undefined
+			particle_data[index] = data;
+			return;
+		}
+
 		particle_data.push_back(data);
+
+		// Add the text_id to the map
+		text_to_id_map.insert({ data.text_id, particle_data.size() - 1 });
 	}
+
+	// Returns -1 if the particle doesn't exist in the map
+	// We could also use an uint32_t and return MAX_UINT_32 instead, we could use the max value as a null value
+	// But for now we're keeping this
+	const int32_t getParticleId(const std::string particle_text_id)
+	{
+		if (text_to_id_map.find(particle_text_id) == text_to_id_map.end()) {
+			return -1;
+		}
+
+		return text_to_id_map[particle_text_id];
+	}
+
 
 	const ParticleData& getParticleData(const uint32_t& index) const {
 // We want performance, and this should never happen in the system, so we only test this in debug
