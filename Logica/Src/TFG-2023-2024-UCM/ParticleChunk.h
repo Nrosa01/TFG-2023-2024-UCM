cd3986d4782b990843ec1bea52b960819e21f938
Rioni <nrosa01@ucm.es>
2023-11-15 17:52:34 +0100

    Implemented chunk class, won't compile

---
+++ Logica/Src/TFG-2023-2024-UCM/ParticleChunk.h
@@ -0,0 +1,93 @@
+﻿#pragma once
+#include <cstdint>
+#include <iterator>
+
+struct Particle;
+struct ParticleDefinitionsHandler;
+
+class ParticleChunkIterator : public std::iterator<std::forward_iterator_tag, Particle>
+{
+private:
+    Particle** current;
+    Particle** end;
+
+public:
+    ParticleChunkIterator(Particle** start, Particle** stop) : current(start), end(stop) {}
+
+    ParticleChunkIterator& operator++()
+    {
+        ++current;
+        return *this;
+    }
+
+    ParticleChunkIterator operator++(int)
+    {
+        ParticleChunkIterator tmp = *this;
+        ++(*this);
+        return tmp;
+    }
+
+    bool operator==(const ParticleChunkIterator& other) const
+    {
+        return current == other.current;
+    }
+
+    bool operator!=(const ParticleChunkIterator& other) const
+    {
+        return !(*this == other);
+    }
+
+    Particle& operator*() const
+    {
+        return **current;
+    }
+};
+
+
+class ParticleChunk
+{
+public:
+	// Constructor
+	ParticleChunk(int width, int height);
+	~ParticleChunk();
+
+private:
+	Particle** chunk_state;
+
+	int width;
+	int height;
+
+	bool clock;
+
+	inline void updateParticle(const uint32_t& x, const uint32_t& y, const  ParticleDefinitionsHandler& registry);
+public:
+	// Getteres
+	inline const int getWidth() const; // Obtiene el ancho de la simulaci�n
+	inline const int getHeight() const; // Obtiene la altura de la simulaci�n
+	inline const Particle& getParticle(int x, int y) const;
+
+	// Const methods
+	inline const bool isInside(uint32_t x, uint32_t y) const;
+	inline const bool isEmpty(const uint32_t& x, const uint32_t& y) const;
+	inline const bool canPush(const uint32_t& other_x, const uint32_t& other_y, const uint32_t& x, const uint32_t& y) const;
+	inline const int getParticleType(const uint32_t& x, const uint32_t& y) const;
+
+	// Side effects methods
+	void update(); // Actualiza el estado de la simulaci�n
+
+	inline void setParticle(uint32_t x, uint32_t y, const Particle& particle); // Coloca una part�cula en la posici�n (x, y)
+	inline bool tryPushParticle(const uint32_t& x, const uint32_t& y, const int& dir_x, const int& dir_y);
+	inline const bool moveParticle(const uint32_t& x, const uint32_t& y, const int& dir_x, const int& dir_y);
+
+    // Iterator
+
+    ParticleChunkIterator begin() const
+    {
+        return ParticleChunkIterator(chunk_state, chunk_state + width * height);
+    }
+
+    ParticleChunkIterator end() const
+    {
+        return ParticleChunkIterator(chunk_state + width * height, chunk_state + width * height);
+    }
+};
--- Logica/Src/TFG-2023-2024-UCM/ParticleSimulation.cpp
+++ Logica/Src/TFG-2023-2024-UCM/ParticleSimulation.cpp
@@ -9,18 +9,13 @@
 #include <iostream>
 #include "Common_utils.h"
 #include "ParticleFactory.h"
+#include "ParticleChunk.h"
 
 static const double PI = 3.1415926535;
 
-ParticleSimulation::ParticleSimulation(int width, int height, int wWidth, int wHeight) : width(width), height(height), wWidth(wWidth), wHeight(wHeight), clock(0), registry(ParticleDefinitionsHandler::getInstance()) {
+ParticleSimulation::ParticleSimulation(int width, int height, int wWidth, int wHeight) : wWidth(wWidth), wHeight(wHeight) {
 
-	chunk_state = new Particle * [width];
-	for (int x = 0; x < width; ++x) {
-		chunk_state[x] = new Particle[height];
-
-		for (int y = 0; y < height; ++y)
-			chunk_state[x][y].type = 0;
-	}
+	chunk = new ParticleChunk(width, height);
 
 	brush_size = width / 20;
 	radius_brush = brush_size / 2;
@@ -32,7 +27,7 @@ ParticleSimulation::ParticleSimulation(int width, int height, int wWidth, int wH
 
 ParticleSimulation::~ParticleSimulation() {
 
-	delete[] chunk_state;
+	delete[] chunk;
 
 }
 
@@ -48,7 +43,7 @@ void ParticleSimulation::initializeTexture()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	// Define el tama�o y el formato de la textura (RGBA)
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, chunk->getWidth(), chunk->getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
 
 	// Establece los datos iniciales de la textura
 	std::fill(textureData.begin(), textureData.end(), 0);
@@ -58,130 +53,32 @@ void ParticleSimulation::initializeTexture()
 
 void ParticleSimulation::updateTexture() {
 	// Recorre los datos de la simulaci�n y actualiza textureData seg�n el estado actual
-	for (int x = 0; x < width; ++x) {
-		for (int y = 0; y < height; y++)
-		{
-			ParticleProject::colour_t c = addGranularity(getParticleData(x, y).particle_color,
-				chunk_state[x][y].random_granularity);
+	const int width = chunk->getWidth();
+	const int height = chunk->getHeight();
+	
+	ParticleDefinitionsHandler& handler = ParticleDefinitionsHandler::getInstance();
 
-			int pos_text = (y * width + x) * 4;
-			textureData[pos_text + 0] = c.r;   // R
-			textureData[pos_text + 1] = c.g;   // G
-			textureData[pos_text + 2] = c.b;   // B
-			textureData[pos_text + 3] = c.a;   // A
-		}
+	for (auto it = chunk->begin(); it != chunk->end(); ++it)
+	{
+		Particle& particle = *it;
+		
+		// Obtener las coordenadas x e y desde el iterador
+		int x = std::distance(chunk->begin(), it) % width;
+		int y = std::distance(chunk->begin(), it) / width;
+
+		ParticleProject::colour_t c = addGranularity(handler.getParticleData(particle.type).particle_color, particle.random_granularity);
+
+		int pos_text = (y * width + x) * 4;
+		textureData[pos_text + 0] = c.r;   // R
+		textureData[pos_text + 1] = c.g;   // G
+		textureData[pos_text + 2] = c.b;   // B
+		textureData[pos_text + 3] = c.a;   // A
 	}
 
 	// Luego, actualiza la textura con los nuevos datos
 	glBindTexture(GL_TEXTURE_2D, textureID);
 	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, textureData.data());
 	glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-const bool ParticleSimulation::isEmpty(const uint32_t& x, const uint32_t& y) const
-{
-	return chunk_state[x][y].type == 0;
-}
-
-const bool ParticleSimulation::moveParticle(const int& dir_x, const int& dir_y, const uint32_t& x, const uint32_t& y)
-{
-	const uint32_t new_x = x + dir_x;
-	const uint32_t new_y = y + dir_y;
-
-	if (isInside(new_x, new_y) && isEmpty(new_x, new_y)) {
-
-		chunk_state[new_x][new_y] = chunk_state[x][y];
-		chunk_state[x][y] = ParticleFactory::createParticle(0);
-
-		return true;
-	}
-	else
-		return false;
-}
-
-inline void ParticleSimulation::updateParticle(const uint32_t& x, const uint32_t& y)
-{
-	const ParticleDefinition& data = getParticleData(x, y);
-	const std::vector<Interaction>& interactions = data.interactions;
-	const uint32_t particle_movement_passes_amount = data.movement_passes.size();
-
-	if (chunk_state[x][y].clock != clock || particle_movement_passes_amount == 0)
-	{
-		chunk_state[x][y].clock = !clock;
-		return; // This particle has already been updated
-	}
-
-	uint32_t particle_movement_passes_index = 0;
-	uint32_t pixelsToMove = 1; // Temporal
-	bool particleIsMoving = true;
-	bool particleCollidedLastIteration = false;
-	uint32_t new_pos_x = x;
-	uint32_t new_pos_y = y;
-
-	while (pixelsToMove > 0 && particleIsMoving)
-	{
-		// Gather particle direction
-		const int32_t dir_x = data.movement_passes[particle_movement_passes_index].x;
-		const int32_t dir_y = data.movement_passes[particle_movement_passes_index].y;
-
-		bool particleMoved = moveParticle(dir_x, dir_y, new_pos_x, new_pos_y);
-
-		// If particle cant move we HAVE to process interactions
-		bool should_break = false;
-		for (const Interaction& interaction : interactions)
-		{
-			// True means simulation can continue, false stops the simulation for the current particle
-			should_break = !interaction.interaction_function(new_pos_x, new_pos_y, particle_movement_passes_index, chunk_state, width, height);
-
-			if (should_break)
-				goto clock_handler;
-		}
-
-		// If interaction didn't cut the update, see if particle didn't move, if so, try pushing
-		// This way we don't have to loop
-		if (!particleMoved)
-			particleMoved = movePushingOtherParticle(dir_x, dir_y, new_pos_x, new_pos_y);
-
-		if (!particleMoved)
-		{
-			// Increment movement pass looping through the passes
-			particle_movement_passes_index++;
-
-			// If we reached the last pass, we reset the index
-			if (particle_movement_passes_index == particle_movement_passes_amount)
-			{
-				particle_movement_passes_index = 0;
-
-				if (particleCollidedLastIteration)
-					particleIsMoving = false;
-
-				particleCollidedLastIteration = true;
-			}
-		}
-		else
-		{
-			particleCollidedLastIteration = false;
-			particleIsMoving = true;
-			pixelsToMove--;
-			new_pos_x += dir_x;
-			new_pos_y += dir_y;
-		}
-	}
-
-clock_handler:
-
-
-	chunk_state[x][y].clock = !clock;
-
-	// Update final position of the particle
-	chunk_state[new_pos_x][new_pos_y].clock = !clock;
-}
-
-const inline ParticleDefinition& ParticleSimulation::getParticleData(const uint32_t& x, const uint32_t& y) const
-{
-	return registry.getParticleData(chunk_state[x][y].type);
-	// This makes the simulation run 7 times slower, just leavint it as a note
-	//return ParticleRegistry::getInstance().getParticleData(chunk_state[x][y].type);
 }
 
 void ParticleSimulation::setMaterial(int mat)
@@ -193,53 +90,14 @@ void ParticleSimulation::setMaterial(int mat)
 // Update from bottom to up
 // We prefer from up to bottom but we're doing this now because it allows us to spot behaviour bugs by sight better
 void ParticleSimulation::update() {
-	for (uint32_t y = 0; y < width; ++y) {
-		for (uint32_t x = 0; x < height; ++x)
-			updateParticle(x, y);
-	}
-
-	clock = !clock;
-
+	chunk->update();
 	updateTexture();
-}
-
-inline const bool ParticleSimulation::canPush(const uint32_t& other_x, const uint32_t& other_y, const uint32_t& x, const uint32_t& y) {
-	return getParticleData(other_x, other_y).properties.density < getParticleData(x, y).properties.density;
-}
-
-bool ParticleSimulation::movePushingOtherParticle(const int& dir_x, const int& dir_y, const uint32_t& x, const uint32_t& y) {
-
-	const uint32_t new_x = x + dir_x;
-	const uint32_t new_y = y + dir_y;
-
-	//this is going to glu glu the performance hehehehe
-	if (isInside(new_x, new_y) && canPush(new_x, new_y, x, y)) {
-		Particle aux = chunk_state[new_x][new_y];
-		chunk_state[new_x][new_y] = chunk_state[x][y];
-		chunk_state[x][y] = ParticleFactory::createParticle(0);
-		//search for an empty space to move
-		for (int i = -5; i < 5; ++i) {
-			for (int j = 1; j < 20; ++j) {
-				if (isInside(new_x + i, new_y + j) && isEmpty(new_x + i, new_y + j)) {
-					chunk_state[new_x + i][new_y + j] = aux;
-					return true;
-				}
-			}
-		}
-		return true;
-	}
-	else
-		return false;
-}
-
-const bool ParticleSimulation::isInside(uint32_t x, uint32_t y) const {
-	return x >= 0 && x < width && y >= 0 && y < height;
 }
 
 void ParticleSimulation::setParticle(uint32_t x, uint32_t y) {
 	// Convierte las coordenadas de pantalla a coordenadas de la simulaci�n
-	int simX = (x * width) / wWidth;
-	int simY = height - (y * height) / wHeight - 1;
+	int simX = (x * chunk->getWidth()) / wWidth;
+	int simY = chunk->getHeight() - (y * chunk->getHeight()) / wHeight - 1;
 	int simX_aux = simX;
 	int simY_aux = simY;
 
@@ -250,24 +108,9 @@ void ParticleSimulation::setParticle(uint32_t x, uint32_t y) {
 			// Id 0 is an special harcoded case now that represents the empty particle. We should do something about that at some point
 			if ((simX_aux * simX_aux + simY_aux * simY_aux) <= (radius_brush * radius_brush) && isInside(i, j) && (isEmpty(i, j) || type_particle == 0))
 			{
-				chunk_state[i][j] = ParticleFactory::createParticle(type_particle);
+				chunk->setParticle(i, j, ParticleFactory::createParticle(type_particle));
 			}
 		}
-}
-
-const bool ParticleSimulation::isParticle(uint32_t x, uint32_t y) const {
-	if (isInside(x, y)) {
-		return  !chunk_state[x][y].type == 0;
-	}
-	return false;
-}
-
-const int ParticleSimulation::getWidth() const {
-	return width;
-}
-
-const int ParticleSimulation::getHeight() const {
-	return height;
 }
 
 void ParticleSimulation::render() {
